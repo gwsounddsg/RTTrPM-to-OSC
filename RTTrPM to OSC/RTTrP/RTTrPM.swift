72313d1b23d8f695a1466daec33f85605f75f72d
GW Rodriguez <grodriguez@feldinc.com>
2019-11-07 15:39:32 -0500

    adds blank RTTrPM file. adds ThirdParty file with Trackable struct using Packet protocol

---
+++ RTTrPM to OSC/RTTrP/RTTrPM.swift
@@ -0,0 +1,13 @@
+//
+//  RTTrPM.swift
+//  RTTrPM to OSC
+//
+//  Created by GW Rodriguez on 11/7/19.
+//  Copyright © 2019 GW Rodriguez. All rights reserved.
+//
+
+import Foundation
+
+
+
+
---
+++ RTTrPM to OSC/RTTrP/ThirdParty.swift
@@ -0,0 +1,36 @@
+//
+//  ThirdParty.swift
+//  RTTrPM to OSC
+//
+//  Created by GW Rodriguez on 11/7/19.
+//  Copyright © 2019 GW Rodriguez. All rights reserved.
+//
+
+import Foundation
+
+
+protocol Packet {
+    var type: uint8 {get}
+    var intSig: uint16 {get} // not sure what this is for
+    var fltSig: uint16 {get} // not sure what this is for
+}
+
+
+struct Trackable: Packet {
+    // Packet
+    let type: uint8
+    let intSig: uint16
+    let fltSig: uint16
+    
+    // Trackable
+    let size: uint16
+    let nameLength: uint8
+    let name: String
+    let modCount: uint8
+    let timestamp: uint32
+
+    
+    func print() {
+        
+    }
+}
