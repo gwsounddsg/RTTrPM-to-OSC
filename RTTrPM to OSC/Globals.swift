1f1c6b4edd72e11ff288813590f697e00e280c99
GW Rodriguez <grodriguez@feldinc.com>
2019-11-08 16:37:33 -0500

    adds Globals file, with printHex for UInt hex printing

---
+++ RTTrPM to OSC/Globals.swift
@@ -0,0 +1,30 @@
+//
+//  Globals.swift
+//  RTTrPM to OSC
+//
+//  Created by GW Rodriguez on 11/8/19.
+//  Copyright © 2019 GW Rodriguez. All rights reserved.
+//
+
+import Foundation
+
+
+protocol HexConverter: BinaryInteger {}
+extension uint8: HexConverter {}
+extension uint16: HexConverter {}
+extension uint32: HexConverter {}
+
+
+/// inserts a print of a UInt8, UInt16, or UInt32 in hex for (eg: 0x3F5B)
+func printHex<T: HexConverter>(_ num: T, bitCount: Int) {
+    switch bitCount {
+    case 8, 16, 32:
+        break
+    default:
+        print("Num Error", terminator: "")
+        return
+    }
+    
+    let str = String(num, radix: 16, uppercase: true)
+    print("0x\(str)", terminator: "")
+}
