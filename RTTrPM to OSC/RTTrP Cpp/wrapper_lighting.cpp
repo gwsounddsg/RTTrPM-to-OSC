b39bb324c6128d03f8bbd4fa82405fecf44a92bc
GW Rodriguez <grodriguez@feldinc.com>
2019-11-21 10:00:32 -0500

    fill out remaining implementation of lighting wrapper, some func's not working

--- RTTrPM to OSC/RTTrP Cpp/wrapper_lighting.cpp
+++ RTTrPM to OSC/RTTrP Cpp/wrapper_lighting.cpp
@@ -20,12 +20,8 @@ MyChannelBlock* newChannelBlock() {
     return reinterpret_cast<MyChannelBlock*>(new ChannelBlock());
 }
 
-MyChannelBlock* newChannelBlockWith(unsigned char* vec, int vecSize, my_uint16 intSig, my_uint16 fltSig) {
-    std::vector<unsigned char> localVec;
-    localVec.assign(vec, vec + vecSize);
-
 #warning second ChannelBlock constructor isn't working
-//    return reinterpret_cast<MyChannelBlock*>(new ChannelBlock(localVec, intSig, fltSig));
+MyChannelBlock* newChannelBlockWith(unsigned char* vec, int vecSize, my_uint16 intSig, my_uint16 fltSig) {
     return newChannelBlock();
 }
 
@@ -76,10 +72,6 @@ MySpot* newSpot() {
 
 #warning second MySpot constructor isn't working
 MySpot* newSpotWith(unsigned char* vec, int vecSize, my_uint16 intSig, my_uint16 fltSig) {
-    std::vector<unsigned char> localVec;
-    localVec.assign(vec, vec + vecSize);
-
-//    return reinterpret_cast<MySpot*>(new Spot(localVec, intSig, fltSig));
     return newSpot();
 }
 
@@ -104,6 +96,7 @@ void spot_channelStruct(MySpot* obj, my_uint16 val) {
     reinterpret_cast<Spot*>(obj)->channelStruct = val;
 }
 
+#warning broken
 void spot_chanBlocks(MySpot* obj, MyChannelBlock* blocks, int blockSize) {
     //std::vector<ChannelBlock*> localBlocks;
     ChannelBlock* localBlocks = reinterpret_cast<ChannelBlock*>(blocks);
@@ -176,6 +169,7 @@ void universe_numSpots(MyUniverse* obj, my_uint16 val) {
     reinterpret_cast<Universe*>(obj)->numSpots = val;
 }
 
+#warning setter isn't working
 void universe_spotList(MyUniverse* obj, MySpot* list, int listSize) {
 //    std::vector<Spot*> localList;
 //
@@ -204,17 +198,170 @@ unsigned long int getUniverseSpotListSize(MyUniverse* obj) {
     return reinterpret_cast<Universe*>(obj)->spotList->size();
 }
 
+#warning not working
 /// Call getUniverseSpotListSize() first to pass spotList with proper malloc'd memory
 void getUniverseSpotList(MyUniverse* obj, MySpot* spotList) {
-//    std::vector<Spot*> uniSpotList = *reinterpret_cast<Universe*>(obj)->spotList;
-//    const unsigned long int listSize = getUniverseSpotListSize(obj);
-//    Spot tempList[listSize];
-//
+    std::vector<Spot*> uniSpotList = *reinterpret_cast<Universe*>(obj)->spotList;
+    const unsigned long int listSize = getUniverseSpotListSize(obj);
+    Spot tempList[listSize];
+
 //    std::copy(uniSpotList.begin(), uniSpotList.end(), tempList);
 
 }
 
 
 void printUniverse(MyUniverse* obj) {
-
+    reinterpret_cast<Universe*>(obj)->printModule();
 }
+
+
+
+
+
+#pragma mark - LightingOutput
+
+MyLightingOutput* newLightingOutput() {
+    return reinterpret_cast<MyLightingOutput*>(new LightingOutput());
+}
+
+#warning second MyLightingOutput constructor isn't working
+MyLightingOutput* newLightingOutputWith(unsigned char* vec, my_uint16 intSig, my_uint16 fltSig) {
+    return newLightingOutput();
+}
+
+void destroyLightingOutput(MyLightingOutput* trash) {
+    delete reinterpret_cast<LightingOutput*>(trash);
+}
+
+
+void lightingOutput_lightSequence(MyLightingOutput* obj, my_uint32 val) {
+    reinterpret_cast<LightingOutput*>(obj)->lightSeuquence = val;
+}
+
+void lightingOutput_holdTime(MyLightingOutput* obj, my_uint32 val) {
+    reinterpret_cast<LightingOutput*>(obj)->holdTime = val;
+}
+
+void lightingOutput_size(MyLightingOutput* obj, my_uint16 val) {
+    reinterpret_cast<LightingOutput*>(obj)->size = val;
+}
+
+void lightingOutput_numUniverses(MyLightingOutput* obj, my_uint16 val) {
+    reinterpret_cast<LightingOutput*>(obj)->numUniverses = val;
+}
+
+void lightingOutput_action(MyLightingOutput* obj, my_uint8 val) {
+    reinterpret_cast<LightingOutput*>(obj)->action = val;
+}
+
+#warning setter isn't working
+void lightingOutput_uniList(MyLightingOutput* obj, MyUniverse* universes) {
+    
+}
+
+
+my_uint32 getLightingOutputLightingSequence(MyLightingOutput* obj) {
+    return reinterpret_cast<LightingOutput*>(obj)->lightSeuquence;
+}
+
+my_uint32 getLightingOutputHoldTime(MyLightingOutput* obj) {
+    return reinterpret_cast<LightingOutput*>(obj)->holdTime;
+}
+
+my_uint16 getLightingOutputSize(MyLightingOutput* obj) {
+    return reinterpret_cast<LightingOutput*>(obj)->size;
+}
+
+my_uint16 getLightingOutputNumUniverses(MyLightingOutput* obj) {
+    return reinterpret_cast<LightingOutput*>(obj)->numUniverses;
+}
+
+my_uint8 getLightingOutputAction(MyLightingOutput* obj) {
+    return reinterpret_cast<LightingOutput*>(obj)->action;
+}
+
+#warning getter isn't working
+unsigned long int getLightingOutputUniList(MyLightingOutput* obj, MyUniverse* list) {
+    std::vector<Universe*> uniList = *reinterpret_cast<LightingOutput*>(obj)->uniList;
+    const unsigned long int size = uniList.size();
+    Universe array[size];
+    
+//    array[0] = uniList[0];
+    
+//    copy(uniList.begin(), uniList.end(), array);
+//    obj = reinterpret_cast<MyLightingOutput*>(array);
+    
+    return size;
+}
+
+
+void printLightingOutput(MyLightingOutput* obj) {
+    reinterpret_cast<LightingOutput*>(obj)->printModule();
+}
+
+
+
+
+
+#pragma mark - MyLightingSync
+
+MyLightingSync* newLightingSync() {
+    return reinterpret_cast<MyLightingSync*>(new LightingSync());
+}
+
+#warning second MyLightingSync constructor isn't working
+MyLightingSync* newLightingSyncWith(unsigned char* vec, my_uint16 intSig, my_uint16 fltSig) {
+    return newLightingSync();
+}
+
+void destroyLightingSync(MyLightingSync* trash) {
+    delete reinterpret_cast<LightingSync*>(trash);
+}
+
+
+void lightingSync_deviceID(MyLightingSync* obj, my_uint32 val) {
+    reinterpret_cast<LightingSync*>(obj)->deviceID = val;
+}
+
+void lightingSync_deviceSubID0(MyLightingSync* obj, my_uint32 val) {
+    reinterpret_cast<LightingSync*>(obj)->deviceSubID0 = val;
+}
+
+void lightingSync_deviceDubID1(MyLightingSync* obj, my_uint32 val) {
+    reinterpret_cast<LightingSync*>(obj)->deviceSubID1 = val;
+}
+
+void lightingSync_seqNum(MyLightingSync* obj, my_uint32 val) {
+    reinterpret_cast<LightingSync*>(obj)->seqNum = val;
+}
+
+void lightingSync_size(MyLightingSync* obj, my_uint16 val) {
+    reinterpret_cast<LightingSync*>(obj)->size = val;
+}
+
+
+my_uint32 getLightingSyncDeviceID(MyLightingSync* obj) {
+    return reinterpret_cast<LightingSync*>(obj)->deviceID;
+}
+
+my_uint32 getLightingSyncDeviceSubID0(MyLightingSync* obj) {
+    return reinterpret_cast<LightingSync*>(obj)->deviceSubID0;
+}
+
+my_uint32 getLightingSyncDeviceSubID1(MyLightingSync* obj) {
+    return reinterpret_cast<LightingSync*>(obj)->deviceSubID1;
+}
+
+my_uint32 getLightingSyncSeqNum(MyLightingSync* obj) {
+    return reinterpret_cast<LightingSync*>(obj)->seqNum;
+}
+
+my_uint16 getLightingSyncSize(MyLightingSync* obj) {
+    return reinterpret_cast<LightingSync*>(obj)->size;
+}
+
+
+void printLightingSync(MyLightingSync* obj) {
+    reinterpret_cast<LightingSync*>(obj)->printModule();
+}
+
